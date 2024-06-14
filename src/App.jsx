2772a138a205bd146285e1f79193ace9549d9f7e
Daniel Neugent <danielneugent@icloud.com>
2024-06-14 11:04:29 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+// <img src={reactLogo}
+/*
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        */
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>testing!</p>
+    </>
+  )
+}
+
+export default App
